f4a4c48efeff1819c6132eb498d9164524439d04
kdshin2010 <kdshin2010@gmail.com>
2016-05-17 12:58:34 -0700

    refactored routes to include json web tokens

--- server/config/routes.js
+++ server/config/routes.js
@@ -4,69 +4,20 @@
 var express = require('express'),
   routes = express.Router(),
   passport = require('passport'),
-  posts = require('../controllers/posts.js'),
-  comments = require('../controllers/comments.js'),
-  User = require('../models/User.js'),
-  Post = require('../models/Post.js');
+  jwt = require('express-jwt'),
+  auth = jwt({
+    secret: 'MY_SECRET',
+    userProperty: 'payload'
+  }),
+  users = require('../Controllers/users.js')
+  posts = require('../Controllers/posts.js'),
+  authentications = require('../Controllers/authentications.js'),
+  profiles = require('../Controllers/profiles.js'),
+  // comments = require('../controllers/comments.js'),
+  User = require('../Models/User.js'),
+  Post = require('../Models/Post.js')
 
 
-routes.post('/user/register', function(req, res) {
-  console.log(req.body)
-  User.register(new User({ username: req.body.username }), req.body.password, function(err, account) {
-    if (err) {
-      return res.status(500).json({err: err});
-    }
-    passport.authenticate('local')(req, res, function () {
-      return res.status(200).json({status: 'Registration successful!'});
-    });
-  });
-});
-
-routes.post('/user/login', function(req, res, next) {
-  passport.authenticate('local', function(err, user, info) {
-    if (err) {
-      return res.status(500).json({err: err});
-    }
-    if (!user) {
-      return res.status(401).json({err: info});
-    }
-    req.logIn(user, function(err) {
-      if (err) {
-        return res.status(500).json({err: 'Could not log in user'});
-      }
-      res.status(200).json({status: 'Login successful!', user: user});
-      console.log(req.user)
-    });
-  })(req, res, next);
-});
-
-routes.get('/userinfo', function(req, res){
-  User.find({_id: req.user._id}, function (err, data) {
-    if(err) {
-      console.log('error!!')
-    } else {
-      res.json(data)
-    }
-  })
-  // console.log(req.user + '***** THIS IS THE REQUEST FOR THE USER *****')
-  // console.log(req.user._id + '!!!!!!!!!!!!!!!!!!!')
-  // console.log(req.user.username + '***************')
-})
-
-routes.get('/user/logout', function(req, res){
-  req.logout();
-  res.status(200).json({status: 'Bye!'});
-});
-
-routes.get('/user/isLoggedIn', function(req, res, next) {
-  if (req.user) {
-    console.log('user is logged in')
-    res.json({message: 'user is logged in'})
-    res.end()
-  } else {
-    console.log('user is not logged in')
-  }
-})
 
 //Testing Client makes API call to backend
 routes.get('/testData'), function(req, res){
